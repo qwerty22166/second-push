a58e0d374cbc54c2559b567689daeeb077f486e7
qwerty22166 <nurjusu184@gmail.com>
2022-07-22 04:00:10 +0600

    Add

---
+++ доп задание/app.js
@@ -0,0 +1,10 @@
+let Num = prompt("Задайте первое число от 0 до 20")
+let nUm = prompt("Задайте второе сисло от 0 до 20")
+let nuM = prompt("Задайте третье сисло от 0 до 20")
+
+let i = 0
+
+for(; i < 21; i++) {
+if(i == Num || i == nUm || i == nuM) continue;
+console.log(i)
+}
