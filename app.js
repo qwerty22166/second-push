c47755e26a39f4cde8305efabf3acf1ed017fa8e
qwerty22166 <nurjusu184@gmail.com>
2022-07-22 02:27:41 +0600

    Add

---
+++ app.js
@@ -0,0 +1,19 @@
+let arr = [
+    {
+        number:23,
+    },
+    {
+        number:72,
+    },
+    {
+        number:73,
+    },
+    {
+        number:24,
+    },
+    {
+        number:15,
+    },
+]
+
+for 
---
+++ доп задание/app.js
@@ -0,0 +1,166 @@
+let arr = [
+    {
+        number: 50,
+    },
+    {
+        number: 49,
+    },
+    {
+        number: 48,
+    },
+    {
+        number: 47,
+    },
+    {
+        number: 46,
+    },
+    {
+        number: 45,
+    },
+    {
+        number: 44,
+    },
+    {
+        number: 43,
+    },
+    {
+        number: 42,
+    },
+    {
+        number: 41,
+    },
+    {
+        number: 40,
+    },
+    {
+        number: 39,
+    },
+    {
+        number: 38,
+    },
+    {
+        number: 37,
+    },
+    {
+        number: 36,
+    },
+    {
+        number: 35,
+    },
+    {
+        number: 34,
+    },
+    {
+        number: 33
+    },
+    {
+        number: 32,
+    },
+    {
+        number: 31,
+    },
+    {
+        number: 30,
+    },
+    {
+        number: 29,
+    },
+    {
+        number: 28,
+    },
+    {
+        number: 27,
+    },
+    {
+        number: 26,
+    },
+    {
+        number: 25,
+    },
+    {
+        number: 24,
+    },
+    {
+        number: 23,
+    },
+    {
+        number: 22,
+    },
+    {
+        number: 21,
+    },
+    {
+        number: 20,
+    },
+    {
+        number: 19,
+    },
+    {
+        number: 18,
+    },
+    {
+        number: 17,
+    },
+    {
+        number: 16,
+    },
+    {
+        number: 15,
+    },
+    {
+        number: 14,
+    },
+    {
+        number: 13,
+    },
+    {
+        number: 12,
+    },
+    {
+        number: 11,
+    },
+    {
+        number: 10,
+    },
+    {
+        number: 9,
+    },
+    {
+        number: 8,
+    },
+    {
+        number: 7,
+    },
+    {
+        number: 6,
+    },
+    {
+        number: 5,
+    },
+    {
+        number: 4,
+    },
+    {
+        number: 3,
+    },
+    {
+        number: 2,
+    },
+    {
+        number: 1,
+    },
+]
+
+// for(let i = 50; i < arr.length; i++) {
+//     console.log(arr[i].number)
+// }
+
+// for(let num of arr){
+//     console.log(num)
+// }
+
+let i = 50;
+
+while(i = 0) {
+    console.log(arr[i].number)
+}
