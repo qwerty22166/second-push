b9ecad58befa0127778957f255674892f4def8e5
qwerty22166 <nurjusu184@gmail.com>
2022-07-22 03:19:27 +0600

    Add

---
+++ 2 задание/app.js
@@ -0,0 +1,6 @@
+let i = 0;
+
+for(; i < 21; i++) {
+    if(i == 5 || i == 10 || i == 15) continue;
+    console.log(i)
+}
--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-let arr = [
+const robots = [
     {
-        number:23,
+    key: "robot1",
+    strength: 250,
     },
     {
-        number:72,
+    key: "robot2",
+    strength: 450,
     },
     {
-        number:73,
+    key: "robot3",
+    strength: 600,
     },
     {
-        number:24,
+    key: "robot4",
+    strength: 130,
     },
     {
-        number:15,
-    },
+    key: "robot5",
+    strength: 1200
+    }
 ]
 
-for 
+const filteredRobots = [];
+
+for(let i = 0; i < robots.length; i++){
+    if (robots[i].strength > 500){
+        filteredRobots.push(robots[i].key, robots[i].strength)
+    }
+
+}console.log(filteredRobots)
