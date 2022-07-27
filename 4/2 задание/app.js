24a2b2c47621d4a79374748d0608bb0d21772ff9
qwerty22166 <nurjusu184@gmail.com>
2022-07-28 01:24:58 +0600

    Add

---
+++ 4/2 задание/app.js
@@ -0,0 +1,10 @@
+function argument(username = 'Andrey, ', text = '26 years old, married, had 2 children'){
+    // console.log("username: " + username);
+    // console.log("text: " + text);
+
+    let sum = username + text;
+    console.log(sum);
+    return sum
+}
+
+argument();
--- 4/app.js
+++ 4/app.js
@@ -1,27 +1,9 @@
-function calcSum(){
-    console.log("Переменная numOme: " + numOne);
-    console.log("Переменная numTwo: " + numTwo);
-
-    let sum = numOne + numTwo;
-    console.log("Сумма: " + sum)
-}
-
 let numOne = prompt("Задайте первое число")
 let numTwo = prompt("Задайте второе число")
 
+function calcSum(numOne, numTwo){
+    console.log("значение первого числа: " + numOne);
+    console.log("значение второгочисла: " + numTwo);
+}
+
 calcSum(numOne, numTwo)
-
-// function calcSum(numOne, numTwo, plus) {
-//     let sum = numOne + numTwo;
-//     if(sum< 1000000000000000000000){
-//         plus();
-//     } else {
-//         console.log("Oшибка повторите заново")
-//     }
-// }
-
-// function showPlus(){
-//     console.log(numOne + numTwo)
-// }
-
-// calcSum(numOne, numTwo, showPlus)
