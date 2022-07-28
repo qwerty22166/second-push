d063fd8f124e89c8e62d9d70d264fab5fdf4c90f
qwerty22166 <nurjusu184@gmail.com>
2022-07-28 23:45:53 +0600

    Add

---
+++ 5/app.js
@@ -0,0 +1,16 @@
+let getSum;
+
+if(2 > 1) {
+let one = prompt("Задайте первое число");
+let two = prompt("Задайте второе число");
+
+        One =+ one;
+        Two =+ two;
+
+    getSum = function() {
+        let sum = One + Two;
+        console.log(sum)
+    }
+}
+
+getSum()
