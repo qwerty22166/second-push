ced38c4c4e363160264386fc18ab457b912473fc
qwerty22166 <nurjusu184@gmail.com>
2022-07-31 20:12:44 +0600

    Add

---
+++ 5/2 задание/app.js
@@ -0,0 +1,8 @@
+let text = prompt("Напиши 1-ое слово");
+let text1 = prompt("Напиши 2-ое слово");
+
+if(2 > 1) {
+    if(text.length > text1.length) {
+        console.log(text)
+    } else {console.log(text1)}
+}
