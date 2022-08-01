4dd20cee0575f11df30242085b4186cb6e141947
qwerty22166 <nurjusu184@gmail.com>
2022-08-01 16:58:13 +0600

    Add

--- 5/2 задание/app.js
+++ 5/2 задание/app.js
@@ -1,8 +1,13 @@
 let text = prompt("Напиши 1-ое слово");
 let text1 = prompt("Напиши 2-ое слово");
 
-if(2 > 1) {
-    if(text.length > text1.length) {
-        console.log(text)
-    } else {console.log(text1)}
-}
+const filtered = []
+
+function getText(text, text1) {
+    let i = 0; i < getText.length; i++;
+    if(text.length > i) {
+                console.log(text)
+            } else {console.log(text1)}
+        }
+
+getText(text, text1)
